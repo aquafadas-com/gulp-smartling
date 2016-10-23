41a3d5b9210c69c8b8cbfa5814509b4f117354e2
Cédric Belin <cedric@belin.io>
2016-10-23 15:26:44 +0200

    Added some documentation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 /**
  * Package entry point.
  */
-import {FileType, RetrievalType} from './core';
 import {DownloadTask, UploadTask} from './tasks';
-
 export {FileType, RetrievalType} from './core';
 
-// TODO
+/**
+ * Downloads the message translations from the Smartling server.
+ * @param {string} filePattern The pattern indicating the target path of the downloaded files.
+ * @param {object} options The task options.
+ * @return {Promise} Completes when the message translations have been downloaded.
+ */
 export function download(filePattern, options) {
   return new DownloadTask(Object.assign({filePattern}, options)).run();
 }
 
-// TODO
+/**
+ * Uploads the message translations to the Smartling server.
+ * @param {string} file The path to the message file.
+ * @param {object} options The task options.
+ * @return {Promise} Completes when the message file has been uploaded.
+ */
 export function upload(file, options) {
   return new UploadTask(Object.assign({file}, options)).run();
 }
