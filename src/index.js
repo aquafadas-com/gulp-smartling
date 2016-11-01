7a0e8c0ba3a33d292e57f3e0720f372af8b5d7db
Cédric Belin <cedric@belin.io>
2016-11-01 01:01:36 +0100

    Task options are optional

--- src/index.js
+++ src/index.js
@@ -4,23 +4,24 @@ export * from './api';
 export * from './file_type';
 export * from './locale';
 export * from './retrieval_type';
+export * from './tasks/index';
 
 /**
  * Downloads the message translations from the Smartling server.
  * @param {string} filePattern The pattern indicating the target path of the downloaded files.
- * @param {object} options The task options.
+ * @param {object} [options] The task options.
  * @return {function} A function allowing to download the message translations.
  */
-export function download(filePattern, options) {
+export function download(filePattern, options = {}) {
   return () => new DownloadTask(Object.assign({filePattern}, options)).run();
 }
 
 /**
  * Uploads the original source content to the Smartling server.
  * @param {string} file The path to the message file.
- * @param {object} options The task options.
+ * @param {object} [options] The task options.
  * @return {function} A function allowing to upload the original source content.
  */
-export function upload(file, options) {
+export function upload(file, options = {}) {
   return () => new UploadTask(Object.assign({file}, options)).run();
 }
